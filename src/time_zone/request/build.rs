485d1917ee42d150ac3a42451081b24328e663bc
Dylan Bowker <dylan.bowker@arkiteq.ca>
2020-01-26 13:16:39 -0700

    Format parameters were in incorrect order.

--- src/time_zone/request/build.rs
+++ src/time_zone/request/build.rs
@@ -17,9 +17,9 @@ impl Request {
 
         let mut query = format!(
             "key={}&location={}&timestamp={}",
+            self.key,
             String::try_from(&self.location).unwrap(),
             self.time.timestamp(),
-            self.key
         );
 
         // This section builds the "optional parameters" portion of the query
