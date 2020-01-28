3d9c3b86d0ecd91a513bfcb994cf303962fa17e3
Dylan Bowker <dylan.bowker@arkiteq.ca>
2020-01-27 23:13:25 -0700

    Changed all f32's to f64's.

--- src/geolocation/response/mod.rs
+++ src/geolocation/response/mod.rs
@@ -8,6 +8,6 @@ use serde::{Serialize, Deserialize};
 #[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub struct Response {
     pub location: Option<LatLng>,
-    pub accuracy: Option<f32>,
+    pub accuracy: Option<f64>,
     pub error: Option<ErrorObject>,
 } // struct
