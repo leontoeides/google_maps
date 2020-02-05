1e9987c50536df1250c8c7fe730e2315d5845037
Dylan Bowker <dylan.bowker@arkiteq.ca>
2020-02-05 14:45:49 -0700

    Positional request methods for single and multiple locations

--- src/elevation/request/for_positional_request.rs
+++ src/elevation/request/for_positional_request.rs
@@ -1,11 +1,36 @@
-use crate::elevation::request::{
-    locations::Locations,
-    Request,
+use crate::{
+    latlng::LatLng,
+    elevation::request::{
+        locations::Locations,
+        Request,
+    }, // elevation::request
 }; // use
 
 impl<'a> Request<'a> {
 
-    /// Adds the _positional request_ parameters to the Elevation API query.
+    /// Adds the _positional request_ parameter to the Elevation API query.
+    ///
+    /// ## Arguments:
+    ///
+    /// * `location` ‧ Defines the location on the earth from which to
+    /// return elevation data. This parameter takes a single `LatLng`
+    /// coordinate.
+    ///
+    /// ## Example:
+    ///
+    /// ```
+    /// // Denver, Colorado, the "Mile High City"
+    /// .for_positional_request(LatLng::try_from(39.7391536, -104.9847034).unwrap())
+    /// ```
+
+    pub fn for_positional_request(&'a mut self, location: LatLng) -> &'a mut Request {
+        // Set the path in Request struct.
+        self.locations = Some(Locations::LatLngs(vec![location]));
+        // Return modified Request struct to caller.
+        self
+    } // fn
+
+    /// Adds a single _positional request_ parameter to the Elevation API query.
     ///
     /// ## Arguments:
     ///
@@ -18,13 +43,15 @@ impl<'a> Request<'a> {
     /// ## Example:
     ///
     /// ```
-    /// .for_positional_request(ElevationLocations::LatLngs(vec![
+    /// .for_positional_requests(ElevationLocations::LatLngs(vec![
     ///     // Denver, Colorado, the "Mile High City"
     ///     LatLng::try_from(39.7391536, -104.9847034).unwrap(),
+    ///     // Death Valley
+    ///     LatLng::try_from(36.23998, -116.83171).unwrap(),
     /// ]))
     /// ```
 
-    pub fn for_positional_request(&'a mut self, locations: Locations) -> &'a mut Request {
+    pub fn for_positional_requests(&'a mut self, locations: Locations) -> &'a mut Request {
         // Set the path in Request struct.
         self.locations = Some(locations);
         // Return modified Request struct to caller.
