43e044749a8a41f49dd0f26bc7748cdc4c08dd73
Dylan Bowker <dylan.bowker@arkiteq.ca>
2022-03-22 16:51:36 -0600

    Initial `Places` API `Place Autcomplete` support.

--- src/places/place_autocomplete/request/with_components.rs
+++ src/places/place_autocomplete/request/with_components.rs
@@ -13,10 +13,12 @@ impl<'a> Request<'a> {
     /// * `component` ‧ A grouping of places to which you would like to restrict
     /// your results. Currently, you can use components to filter by up to 5
     /// countries.
+    ///
+    /// * Multiple components may be stacked together.
 
     pub fn with_component(&'a mut self, component: Country) -> &'a mut Request {
         // Set components in Request struct.
-        self.components = vec![component];
+        self.components.extend(vec![component]);
         // Return modified Request struct to caller.
         self
     } // fn
@@ -35,10 +37,12 @@ impl<'a> Request<'a> {
     /// * `components` ‧ A grouping of places to which you would like to restrict
     /// your results. Currently, you can use components to filter by up to 5
     /// countries.
+    ///
+    /// * Multiple components may be stacked together.
 
     pub fn with_components(&'a mut self, components: Vec<Country>) -> &'a mut Request {
         // Set components in Request struct.
-        self.components = components;
+        self.components.extend(components);
         // Return modified Request struct to caller.
         self
     } // fn
--- src/places/place_autocomplete/request/with_types.rs
+++ src/places/place_autocomplete/request/with_types.rs
@@ -16,10 +16,39 @@ impl<'a> Request<'a> {
     /// a single type is allowed. The exception is that you can safely mix the 
     /// geocode and establishment types, but note that this will have the same 
     /// effect as specifying no types.
+    ///
+    /// * Multiple result type filters may be stacked together.
+
+    pub fn with_type(&'a mut self, autocomplete_type: AutocompleteType) -> &'a mut Request {
+        // Set types in Request struct.
+        self.types.extend(vec![autocomplete_type]);
+        // Return modified Request struct to caller.
+        self
+    } // fn
+
+} // impl
+
+// -----------------------------------------------------------------------------
+
+impl<'a> Request<'a> {
+
+    /// Adds the types parameter to the Place API _Place Autocomplete_ query.
+    ///
+    /// ## Arguments:
+    ///
+    /// * `types` ‧ You may restrict results from a Place Autocomplete request to
+    /// be of a certain type by passing a types parameter. The parameter
+    /// specifies a type or a type collection, as listed in the supported types
+    /// below. If nothing is specified, all types are returned. In general only
+    /// a single type is allowed. The exception is that you can safely mix the
+    /// geocode and establishment types, but note that this will have the same
+    /// effect as specifying no types.
+    ///
+    /// * Multiple result type filters may be stacked together.
 
     pub fn with_types(&'a mut self, types: Vec<AutocompleteType>) -> &'a mut Request {
         // Set types in Request struct.
-        self.types = types;
+        self.types.extend(types);
         // Return modified Request struct to caller.
         self
     } // fn
